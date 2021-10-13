30ec739ee30b2e99b5c713477b12fb1f5afb1049
今野龍之介 <hilcrhymer78787@gmail.com>
2021-10-13 21:04:22 +0900

    test

---
+++ go/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+  "database/sql"
+  "log"
+  "net/http"
+
+  _ "github.com/go-sql-driver/mysql"
+  "github.com/labstack/echo"
+)
+
+type Opening struct {
+  ID   int    `json:"id"`
+  Name string `json:"name"`
+}
+
+func main() {
+  e := echo.New()
+  e.GET("/opening/read", read)
+  e.GET("/opening/allread", allread)
+  // e.GET("/test", test)
+  e.Start("localhost:1323")
+}
+
+func read(c echo.Context) error {
+
+  // データベース接続処理
+  db, err := sql.Open("mysql", "root@/test")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer db.Close()
+
+  id := c.QueryParam("id")
+  var name string
+  err = db.QueryRow("SELECT name FROM opening WHERE opening_id = ?", id).Scan(&name)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  data := map[string]string{
+    "id":   id,
+    "name": name,
+  }
+
+  return c.JSON(http.StatusOK, data)
+}
+
+func allread(c echo.Context) error {
+
+  // データベース接続処理
+  db, err := sql.Open("mysql", "root:@/test")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer db.Close()
+
+  rows, err := db.Query("SELECT * FROM opening")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  openingArray := make([]Opening, 0)
+  for rows.Next() {
+    var opening Opening
+    err := rows.Scan(&opening.ID, &opening.Name)
+    if err != nil {
+      log.Fatal(err)
+    }
+    openingArray = append(openingArray, opening)
+  }
+  return c.JSON(http.StatusOK, openingArray)
+}
+
+// func test(c echo.Context) error {
+
+//  // データベース接続処理
+//  db, err := sql.Open("mysql", "root@/test")
+//  if err != nil {
+//    log.Fatal(err)
+//  }
+//  defer db.Close()
+
+//  db.Query("SELECT * FROM opening")
+//  return c.JSON(http.StatusOK, "rows")
+
+// }
