b67d8a43f19505086b2b89689080d75b003b3e51
今野龍之介 <hilcrhymer78787@gmail.com>
2021-10-14 09:14:28 +0900

    test

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="crud">
-        <form @submit.prevent="createTodo">
-            <input v-model="newtodo" type="text">
-            <v-btn @click="createTodo()" color="orange" dark>create</v-btn>
+        <form @submit.prevent="createTask">
+            <input v-model="newTask" type="text">
+            <v-btn @click="createTask()" color="orange" dark>create</v-btn>
         </form>
         <hr>
         <ul>
@@ -13,7 +13,7 @@
             <li v-for="(task, index) in tasks" :key="index" class="mb-3">
                 <span class="pr-5">{{task.id}}</span>
                 <span class="pr-5">{{task.name}}</span>
-                <v-btn @click="deleteTodo()" color="error" dark>delete</v-btn>
+                <v-btn @click="deleteTask()" color="error" dark>delete</v-btn>
             </li>
         </ul>
     </div>
@@ -23,30 +23,33 @@
 export default {
     data() {
         return {
-            newtodo: "",
+            newTask: "",
             tasks: [],
         };
     },
     methods: {
-        createTodo() {
-            alert("要素が追加されました");
-            this.newtodo = "";
+        readTask() {
+            this.$axios
+                .get(`/opening/allread`)
+                .then((res) => {
+                    this.tasks = res.data;
+                })
+                .catch((err) => {
+                    alert("通信に失敗しました");
+                });
         },
-        deleteTodo() {
-            if (confirm("「" + '内容' + "」を削除しますか？")) {
+        createTask() {
+            alert("要素が追加されました");
+            this.newTask = "";
+        },
+        deleteTask() {
+            if (confirm("「" + "内容" + "」を削除しますか？")) {
                 alert("要素が削除されました");
             }
         },
     },
     mounted() {
-        this.$axios
-            .get(`/opening/allread`)
-            .then((res) => {
-                this.tasks = res.data
-            })
-            .catch((err) => {
-                alert("通信に失敗しました");
-            });
+        this.readTask();
     },
 };
 </script>
@@ -54,7 +57,10 @@ export default {
 hr {
     margin: 20px 0;
 }
-li{
+li {
     list-style: none;
+}
+input {
+    border: 1px solid orange;
 }
 </style>
