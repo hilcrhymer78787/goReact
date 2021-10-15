3fb10a696719e1d4f08a769f4a653659dbd9b78e
今野龍之介 <hilcrhymer78787@gmail.com>
2021-10-15 09:09:55 +0900

    test

---
+++ go/pkg/db/connect.go
@@ -0,0 +1,15 @@
+package connect
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Connect() *sql.DB {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/test")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	return db
+}
